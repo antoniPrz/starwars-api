17d4b124392ccd9024cedf29793e0d22077260af
antoni perez <antoni.protoni@gmail.com>
2021-03-18 21:06:43 -0300

    test routerç

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Details from './views/Details';
+import Home from './views/Home'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+			
+			<Switch>
+				<Route exact path="/details">
+					<Details />
+				</Route>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route render={() => <h1>Not found</h1>} />
+			</Switch>
+		</Router>
+  )
 }
-
-export default App;
